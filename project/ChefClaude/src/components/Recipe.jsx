067a00170e51700c4358dadd1b55a48f959b36b5
Ami0408 <155718814+Ami0408@users.noreply.github.com>
2025-05-19 18:03:37 +0100

    Update Recipe.jsx

--- project/ChefClaude/src/components/Recipe.jsx
+++ project/ChefClaude/src/components/Recipe.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import ReactMarkdown from 'react-markdown'
+import Markdown from 'react-markdown'
 
 export default function Recipe ({ generated }){
   return(
     <section>
       <h2> Chef Claude Recommends </h2>
-      <ReactMarkdown> { generated } </ReactMarkdown>
+      <Markdown> {generated} </Markdown>
     </section>
   );
 }
